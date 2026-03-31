fc2fec7e86c24991ed8398ee67b7db7a319aba05
sreenithiya958-bit <sreenithiya958@gmail.com>
2026-03-31 22:56:20 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,33 @@
+var name ="nithiyasree";
+let mark = 95;
+const country ="INDIA";
+let isIndian ="true";
+let result="null";
+
+
+let a = 20
+let b = 5
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+if(a == "20"){
+    console.log('a is equal to 20')
+}
+if(a ==="20"){
+    console.log('a is equal to 20 ')
+}
+else{
+    console.log('a is not equal to 20')
+}
+
+
+x=true
+y=false
+if(x && y){
+    console.log("it is true");
+}
+else{
+    console.log('it is false');
+}
