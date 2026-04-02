1472a15c1890b5064e8909bd30b69dd94afe620d
sreenithiya958-bit <sreenithiya958@gmail.com>
2026-04-02 21:05:17 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,36 @@
+let n = -5;
+if(n > 0){
+    console.log("n is positive")
+}
+else{
+    console.log("n is negative")
+}
+
+
+let days = 4;
+switch(days){
+    case 1:
+        console.log("sunday");
+        break;
+    case 2:
+        console.log("monday");
+        break;
+    case 3:
+        console.log("tuesday");
+        break;
+    case 4:
+        console.log("wednesday");
+        break;
+    case 5:
+        console.log("thursday");
+        break;
+    case 6:
+        console.log("friday");
+        break;
+    case 7:
+        console.log("saturday");
+    default:
+        console.log("no days");
+        break;        
+}
+      
