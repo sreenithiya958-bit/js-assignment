c98b7f08da5af0927063acc3d3e5f7f064988cce
sreenithiya958-bit <sreenithiya958@gmail.com>
2026-04-02 22:05:42 +0530

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,32 @@
+//2 nd question
+
+let i = 1;
+while(i <= 5){
+    console.log(i);
+    i++
+}
+
+// 1 st question
+for(let i =1; i<=10; i++){
+    console.log(i);
+}
+
+// 3 th questin
+let n = 1;
+do{
+    console.log(n);
+    n++;
+}while(n <=3);
+
+
+// 4 th question
+
+for(let i = 1; i<= 10; i++){
+    if(i === 5){
+        continue;
+    }
+    if( i === 8){
+        break;
+    }
+    console.log(i);
+}
