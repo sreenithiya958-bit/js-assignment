6963a421df9e02bdcdace2052d8215507340b8b4
sreenithiya958-bit <sreenithiya958@gmail.com>
2026-04-08 20:47:58 +0530

    Add files via upload

--- task1.js
+++ task1.js
@@ -1,36 +1,19 @@
-let n = -5;
-if(n > 0){
-    console.log("n is positive")
-}
-else{
-    console.log("n is negative")
-}
+let arr = [1,2,3,4,5,6]
+let doubled = arr.map(item => item*2);
+console.log(doubled)
 
+let num = [1,2,3,4,5,6]
+let evennum = num.filter(item => item %2 === 0);
+console.log(evennum);
 
-let days = 4;
-switch(days){
-    case 1:
-        console.log("sunday");
-        break;
-    case 2:
-        console.log("monday");
-        break;
-    case 3:
-        console.log("tuesday");
-        break;
-    case 4:
-        console.log("wednesday");
-        break;
-    case 5:
-        console.log("thursday");
-        break;
-    case 6:
-        console.log("friday");
-        break;
-    case 7:
-        console.log("saturday");
-    default:
-        console.log("no days");
-        break;        
-}
-      
+let arr3 = [5,6,7,3,9]
+let sum = arr3.reduce((total,item) => total + item );
+console.log(sum)
+
+let arr1 = [3,,9,12,5,16,23]
+let result = arr1.find(item => item > 10)
+console.log(result);
+
+let arr2 = [10,15,23,20,51]
+let check = arr2.includes(23);
+console.log(check);
