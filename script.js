3ef3023cb28d59c43637a4dea98f54742d205485
sreenithiya958-bit <sreenithiya958@gmail.com>
2026-04-15 21:50:28 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-console.log("Name : Nithiyasree")
-console.log("Age : 21")
+// update
+let constant = 'nithiya';
+let age = 20
+age = 21
+console.log(age)
+
+//template literals
+const Name = 'nithiya'
+const department = 'AI&DS'
+const message = `hello, i am ${Name} and i'm a ${department} student`
+console.log(message)
+
+// deafult parameter
+function greet (name ="guest" ){
+    console.log(`hello, ${name}`);
+}
+greet();
+greet("sree");
